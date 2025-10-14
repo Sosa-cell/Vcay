dcb6b5794e6649d0b1b44cd6d627a653080834ac
Sosa-cell <sosaguobadia@gmail.com>
2025-10-13 23:38:40 -0500

    Require email for user registration

--- booking/views.py
+++ booking/views.py
@@ -5,19 +5,35 @@ from django.contrib.auth.decorators import login_required
 
 from django.utils import timezone
 from datetime import date
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
 
 def register(request):
-	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			return redirect('house_list')
-	else:
-		form = UserCreationForm()
-	return render(request, 'booking/register.html', {'form': form})
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('house_list')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'booking/register.html', {'form': form})
 
 def house_list(request):
 	houses = BeachHouse.objects.all()

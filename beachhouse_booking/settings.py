266e518afdf872033415c20e5ec28a1ab25df09e
Sosa-cell <sosaguobadia@gmail.com>
2025-10-13 23:29:42 -0500

    Add password reset functionality and templates

--- beachhouse_booking/settings.py
+++ beachhouse_booking/settings.py
@@ -129,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- booking/urls.py
+++ booking/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('house/<int:house_id>/book/', views.book_house, name='book_house'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='booking/login.html'), name='login'),
+    # Password reset URLs
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='booking/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='booking/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='booking/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='booking/password_reset_complete.html'), name='password_reset_complete'),
 ]

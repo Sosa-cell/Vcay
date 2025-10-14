aad949763112413e04e65fd45de9b4d77c0d1086
Sosa-cell <sosaguobadia@gmail.com>
2025-10-13 23:17:05 -0500

    Add user login page and update navigation/settings for LOGIN_URL

--- booking/urls.py
+++ booking/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.house_list, name='house_list'),
     path('house/<int:house_id>/', views.house_detail, name='house_detail'),
     path('house/<int:house_id>/book/', views.book_house, name='book_house'),
     path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='booking/login.html'), name='login'),
 ]
